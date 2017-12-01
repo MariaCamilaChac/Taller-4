628ec3e91a70b25396627266e12ba71128b66f87
MariaCamilaChac <32178494+MariaCamilaChac@users.noreply.github.com>
2017-12-01 09:47:50 -0500

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -5,7 +5,7 @@
 /*
 *
 * Estructura de Datos- punto 3
-*	@author :Camila chacón
+*	@author :Camila chacÃ³n
 *	@date 28-11-2017
 *	@version 3
 *
@@ -14,7 +14,7 @@
  
  using namespace std;
  
-  int arreglo(int array[], int tam){
+  int arreglo(int array[], int tam){ // create the method for print the array
   	
   	for(int i = 0 ; i < tam ; i++){
  	
@@ -23,7 +23,7 @@
 	 }
   }
   
-  int insertionSort ( int array [], int tam){
+  int insertionSort ( int array [], int tam){  // create the ordering method
   	int x;
   	int index;
   	for ( int i = 0; i < tam ; i++){
@@ -48,25 +48,25 @@
  	int y;
  	
  	string str;
-    vector<int> vect;
+    vector<int> vect; // bring the vector library 
     
-    cout <<"ingrese el tamaño del arreglo: " << endl;
+    cout <<"ingrese el tamaÃ±o del arreglo: " << endl; // ask the length of array
  	cin >> tam;
-    int array[tam];
-    cout << "Digite los números: \n";
+    int array[tam];  // create the principal array
+    cout << "Digite los nÃºmeros: \n";
     cin >> str;
     stringstream ss(str);
     int i;
     
     while (ss >> i){
-        vect.push_back(i);
-        if (ss.peek() == ',')
-            ss.ignore();
+        vect.push_back(i); //Value to be copied (or moved) to the new element.
+        if (ss.peek() == ',') //Returns the next character in the input sequence, without extracting it
+            ss.ignore(); //Extracts characters from the input sequence and discards them
     }
     for (i=0; i< vect.size(); i++){
-    	array[i] = vect.at(i);
+    	array[i] = vect.at(i);  //assign the new vector in array without comas
 	}
-    
+    // call the methods
 	arreglo(array,tam);
 	cout << "insertion Sort"<< endl;
 	insertionSort(array,tam);
